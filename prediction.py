6b9b243213c051f088af12df12d90f5c8ec332ae
oaarnikoivu <oliver.aarnikoivu@outlook.com>
2020-07-24 09:28:47 +0200

    testing using logreg

--- prediction.py
+++ prediction.py
@@ -1,46 +1,41 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
 from sklearn.multiclass import OneVsRestClassifier
-from sklearn.metrics import f1_score, roc_auc_score
+from sklearn.metrics import f1_score, roc_auc_score, classification_report
 
 df = pd.read_csv('/Users/olive/github/fantasynba/data/test.csv')
+
 df.drop(['PLAYER', 'POS'], axis=1, inplace=True)
 
 X = df[df.columns[0:10]]
 y = df[df.columns[10:18]]
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.1, random_state=43)
+ben_simmons = [[35.7, 0.59, 0.62, 0, 15.7, 7.9, 8.2, 2.1, 0.6, 3.6]]
+giannis = [[33.0, 0.53, 0.58, 0.1, 17.7, 15.2, 2.7, 1.9, 1.6, 3.6]]
+drummond = [[33.0, 0.53, 0.58, 0.1, 17.7, 15.2, 2.7, 1.9, 1.6, 3.6]]
+john_collins = [[33.2, 0.58, 0.78, 1.4, 21.6, 10.1, 1.5, 0.8, 1.6, 1.8]]
 
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=43)
 
 CATEGORIES = ['PUNT_FG', 'PUNT_TO', 'PUNT_AST', 'PUNT_3PM',
               'PUNT_FT', 'PUNT_BLK', 'PUNT_TREB', 'PUNT_STL']
 
 classifier = OneVsRestClassifier(
-    LogisticRegression(solver="sag", n_jobs=1, max_iter=5000))
+    LogisticRegression(solver="lbfgs", n_jobs=1, max_iter=5000))
 
-preds_list = []
-labels_list = []
+classifier.fit(X_train, y_train)
+predictions = classifier.predict(X_test)
+print(f'{roc_auc_score(y_test, predictions):.2f}')
 
-for cat in CATEGORIES:
-    print('\n... Processing {}'.format(cat))
 
-    tr_y = y_train[cat]
-    ts_y = y_test[cat]
+def do_predict(player):
+    predictions = classifier.predict(player)
+    for i in range(len(predictions[0])):
+        if predictions[0][i] == 1:
+            print(f'{CATEGORIES[i]}')
 
-    labels_list.append(ts_y)
 
-    classifier.fit(X_train, tr_y)
-    y_pred_X = classifier.predict(X_train)
-
-    test_y = classifier.predict(X_test)
-    preds_list.append(test_y)
-
-    print(f'F1 Score: {f1_score(ts_y, test_y):.2f}')
-    print(f'Accuracy: {roc_auc_score(ts_y, test_y):.2f}')
-
-print('')
-print(f'F1 Micro: {f1_score(labels_list, preds_list, average="micro")}')
-print(f'F1 Macro: {f1_score(labels_list, preds_list, average="macro")}')
-print(f'ROC AUC: {roc_auc_score(labels_list, preds_list):.2f}')
+do_predict(john_collins)
